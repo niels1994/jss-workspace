33bb9a3ffda421435a073cb87c783ca70ae60292
Voigt, Tom <ui420627@rwe.com>
2022-10-26 11:50:18 +0200

    bump ng-packagr and add exports to public api

--- packages/sitecore-jss-angular/src/public_api.ts
+++ packages/sitecore-jss-angular/src/public_api.ts
@@ -1,11 +1,53 @@
+export { constants, enableDebug, HttpDataFetcher, HttpResponse } from '@sitecore-jss/sitecore-jss';
+export {
+  DictionaryService,
+  GraphQLDictionaryService,
+  RestDictionaryService,
+} from '@sitecore-jss/sitecore-jss/i18n';
+export {
+  ComponentFields,
+  ComponentParams,
+  ComponentRendering,
+  Field,
+  getChildPlaceholder,
+  getFieldValue,
+  GraphQLLayoutService,
+  HtmlElementRendering,
+  LayoutService,
+  LayoutServiceContextData,
+  LayoutServiceData,
+  PlaceholdersData,
+  RestLayoutService,
+  RouteData,
+} from '@sitecore-jss/sitecore-jss/layout';
+export { mediaApi } from '@sitecore-jss/sitecore-jss/media';
+export {
+  CampaignInstance,
+  EventInstance,
+  GoalInstance,
+  OutcomeInstance,
+  PageViewInstance,
+  trackingApi,
+  TrackingRequestOptions,
+} from '@sitecore-jss/sitecore-jss/tracking';
+export {
+  handleEditorAnchors,
+  isEditorActive,
+  isServer,
+  resetEditorChromes,
+} from '@sitecore-jss/sitecore-jss/utils';
+export { DateDirective } from './components/date.directive';
 export { FileDirective } from './components/file.directive';
+export { GenericLinkDirective } from './components/generic-link.directive';
+export { HiddenRenderingComponent } from './components/hidden-rendering.component';
 export { ImageDirective } from './components/image.directive';
 export { LinkDirective } from './components/link.directive';
-export { RouterLinkDirective } from './components/router-link.directive';
-export { GenericLinkDirective } from './components/generic-link.directive';
-export { PlaceholderComponent } from './components/placeholder.component';
 export { PlaceholderLoadingDirective } from './components/placeholder-loading.directive';
+export { PlaceholderComponent } from './components/placeholder.component';
 export { ComponentNameAndType, DYNAMIC_COMPONENT } from './components/placeholder.token';
+export { RenderComponentComponent } from './components/render-component.component';
+export { RenderEachDirective } from './components/render-each.directive';
+export { RenderEmptyDirective } from './components/render-empty.directive';
 export { isRawRendering } from './components/rendering';
 export {
   FileField,
@@ -16,43 +58,6 @@ export {
   TextField,
 } from './components/rendering-field';
 export { RichTextDirective } from './components/rich-text.directive';
+export { RouterLinkDirective } from './components/router-link.directive';
 export { TextDirective } from './components/text.directive';
 export { JssModule } from './lib.module';
-export { mediaApi } from '@sitecore-jss/sitecore-jss/media';
-export {
-  DictionaryService,
-  GraphQLDictionaryService,
-  RestDictionaryService,
-} from '@sitecore-jss/sitecore-jss/i18n';
-export {
-  LayoutService,
-  LayoutServiceData,
-  LayoutServiceContextData,
-  GraphQLLayoutService,
-  RestLayoutService,
-  PlaceholdersData,
-  RouteData,
-  Field,
-  HtmlElementRendering,
-  getChildPlaceholder,
-  getFieldValue,
-  ComponentRendering,
-  ComponentFields,
-  ComponentParams,
-} from '@sitecore-jss/sitecore-jss/layout';
-export { constants, HttpDataFetcher, HttpResponse, enableDebug } from '@sitecore-jss/sitecore-jss';
-export {
-  isServer,
-  isEditorActive,
-  resetEditorChromes,
-  handleEditorAnchors,
-} from '@sitecore-jss/sitecore-jss/utils';
-export {
-  trackingApi,
-  TrackingRequestOptions,
-  CampaignInstance,
-  GoalInstance,
-  OutcomeInstance,
-  EventInstance,
-  PageViewInstance,
-} from '@sitecore-jss/sitecore-jss/tracking';
